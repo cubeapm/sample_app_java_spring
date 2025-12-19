8f2f3670b74d9bd016afcc16bbd5c81806802051
ishaan766 <ishaan766@gmail.com>
2025-12-19 16:02:41 +0530

    added logging library

--- src/main/java/sample_app_java_spring/sampleappjava/app.java
+++ src/main/java/sample_app_java_spring/sampleappjava/app.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.cache.annotation.Cacheable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class app {
@@ -21,35 +23,44 @@ public class app {
     @Autowired
     private RestTemplate restTemplate;
 
+    private static final Logger logger = LoggerFactory.getLogger(app.class);
+
 
     @RequestMapping("/")
     public String index(){
+        logger.info("Index endpoint called");
         return "Hello";
     }
 
     @RequestMapping("/exception")
     public String throwException() {
+        logger.info("Exception occuerred");
         throw new RuntimeException("Exception");
         }
 
     @GetMapping("/param/{param}")
     public String getParam(@PathVariable String param) {
+        logger.info("param called");
         return "Got param " + param;
     }
 
     @GetMapping("/api")
     public String api() {
+        logger.info("api called calling external service");
         String response = restTemplate.getForObject("http://localhost:8000", String.class);
+        logger.info("api response received");
         return "API called: " + response;
     }
 
     // curl http://localhost:8080/mysql/add -d name="xyz" -d email="xyz@gmail.com"
     @PostMapping("/mysql/add")
     public @ResponseBody User addNewUser(@RequestParam String name , @RequestParam String email) {
+        logger.info("mysql called to add user");
         User springUser = new User();
         springUser.setName(name);
         springUser.setEmail(email);
         userRepository.save(springUser);
+        logger.info("User saved successfully");
         return springUser;
     }
 
@@ -57,6 +68,7 @@ public class app {
     // curl http://localhost:8080/all
     @GetMapping("/all")
     public @ResponseBody Iterable<User> getAllUsers() {
+        logger.info("all users called");
         return userRepository.findAll();
     }
 
@@ -64,6 +76,7 @@ public class app {
     @GetMapping("/redis")
     @Cacheable("redisCache")
     public String getCachedRedisMessage() {
+        logger.info("redis called");
         return "Redis called";
     }
 }
